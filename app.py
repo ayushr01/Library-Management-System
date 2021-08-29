078c43c170d3a2705d1630a05bb3595b89d4befd
Ayush Rao <ayushvijayrao@gmail.com>
2021-08-29 17:40:59 +0530

    Added HiDPI support

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import Utils.foldermaker  # Creates the folder for DB and pwd
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from Utils.primarywindow import MainWindow
+
+# Adds HiDPI support to the app
+QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
 
 app = QApplication([])
 
