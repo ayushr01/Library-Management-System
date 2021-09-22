0b5c932407a6594b6b0891f152e0f5ab67f02576
Ayush Rao <ayushvijayrao@gmail.com>
2021-09-22 16:33:17 +0530

    Added PassThrough rounding policy

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from Utils.primarywindow import MainWindow
 # Adds HiDPI support to the app
 QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
 QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
+QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+# https://doc.qt.io/qt-5/qt.html#HighDpiScaleFactorRoundingPolicy-enum
+
 
 app = QApplication([])
 
