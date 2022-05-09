0c7f2f1ab29dea20e787331e939e61af1e8912c5
Ayush Rao <ayushvijayrao@gmail.com>
2022-05-10 00:43:34 +0530

    Updated delete member dialog

--- UI/deletemembersdialog.py
+++ UI/deletemembersdialog.py
@@ -12,66 +12,62 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 class Ui_deletememdialog(object):
     def setupUi(self, deletememdialog):
         deletememdialog.setObjectName("deletememdialog")
-        deletememdialog.resize(500, 230)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Fixed, QtWidgets.QSizePolicy.Policy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(deletememdialog.sizePolicy().hasHeightForWidth())
-        deletememdialog.setSizePolicy(sizePolicy)
-        deletememdialog.setMinimumSize(QtCore.QSize(500, 230))
-        deletememdialog.setMaximumSize(QtCore.QSize(500, 230))
-        font = QtGui.QFont()
-        font.setPointSize(13)
-        font.setBold(True)
-        deletememdialog.setFont(font)
+        deletememdialog.resize(400, 200)
+        deletememdialog.setMinimumSize(QtCore.QSize(400, 200))
+        deletememdialog.setMaximumSize(QtCore.QSize(400, 200))
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(deletememdialog)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
         self.memlist = QtWidgets.QListWidget(deletememdialog)
-        self.memlist.setGeometry(QtCore.QRect(10, 10, 341, 181))
         font = QtGui.QFont()
         font.setPointSize(13)
-        font.setBold(False)
         self.memlist.setFont(font)
         self.memlist.setEditTriggers(QtWidgets.QAbstractItemView.EditTrigger.NoEditTriggers)
         self.memlist.setObjectName("memlist")
+        self.horizontalLayout.addWidget(self.memlist)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.deletebutton = QtWidgets.QPushButton(deletememdialog)
+        self.deletebutton.setMinimumSize(QtCore.QSize(0, 35))
+        self.deletebutton.setStyleSheet("QPushButton#deletebutton{\n"
+"    background-color: red;\n"
+"    border-radius: 5px;\n"
+"    color: white;\n"
+"}\n"
+"\n"
+"QPushButton#deletebutton:hover{\n"
+"    border: 2px solid teal;\n"
+"}")
+        self.deletebutton.setObjectName("deletebutton")
+        self.verticalLayout.addWidget(self.deletebutton)
+        self.closebutton = QtWidgets.QPushButton(deletememdialog)
+        self.closebutton.setMinimumSize(QtCore.QSize(0, 35))
+        self.closebutton.setStyleSheet("QPushButton#closebutton{\n"
+"    background-color: #656565;\n"
+"    border-radius: 5px;\n"
+"    color: white;\n"
+"}\n"
+"\n"
+"QPushButton#closebutton:hover{\n"
+"    border: 2px solid teal;\n"
+"}")
+        self.closebutton.setObjectName("closebutton")
+        self.verticalLayout.addWidget(self.closebutton)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.horizontalLayout.setStretch(0, 3)
+        self.horizontalLayout.setStretch(1, 1)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
         self.errorlabel = QtWidgets.QLabel(deletememdialog)
-        self.errorlabel.setGeometry(QtCore.QRect(10, 200, 481, 22))
         font = QtGui.QFont()
-        font.setPointSize(13)
         font.setBold(True)
         self.errorlabel.setFont(font)
-        self.errorlabel.setStyleSheet("color: red")
+        self.errorlabel.setStyleSheet("color: orange;")
         self.errorlabel.setText("")
         self.errorlabel.setObjectName("errorlabel")
-        self.layoutWidget = QtWidgets.QWidget(deletememdialog)
-        self.layoutWidget.setGeometry(QtCore.QRect(360, 10, 132, 181))
-        font = QtGui.QFont()
-        font.setPointSize(13)
-        font.setBold(True)
-        self.layoutWidget.setFont(font)
-        self.layoutWidget.setObjectName("layoutWidget")
-        self.buttons = QtWidgets.QVBoxLayout(self.layoutWidget)
-        self.buttons.setContentsMargins(0, 0, 0, 0)
-        self.buttons.setSpacing(10)
-        self.buttons.setObjectName("buttons")
-        self.deletebutton = QtWidgets.QPushButton(self.layoutWidget)
-        self.deletebutton.setMinimumSize(QtCore.QSize(130, 50))
-        font = QtGui.QFont()
-        font.setPointSize(13)
-        font.setBold(True)
-        self.deletebutton.setFont(font)
-        self.deletebutton.setStyleSheet("background-color : red;\n"
-"color: white;")
-        self.deletebutton.setObjectName("deletebutton")
-        self.buttons.addWidget(self.deletebutton)
-        self.closebutton = QtWidgets.QPushButton(self.layoutWidget)
-        self.closebutton.setMinimumSize(QtCore.QSize(130, 50))
-        font = QtGui.QFont()
-        font.setPointSize(13)
-        font.setBold(True)
-        self.closebutton.setFont(font)
-        self.closebutton.setStyleSheet("background-color: #333333;\n"
-"color: white;")
-        self.closebutton.setObjectName("closebutton")
-        self.buttons.addWidget(self.closebutton)
+        self.verticalLayout_2.addWidget(self.errorlabel)
+        self.verticalLayout_2.setStretch(0, 8)
+        self.verticalLayout_2.setStretch(1, 1)
 
         self.retranslateUi(deletememdialog)
         QtCore.QMetaObject.connectSlotsByName(deletememdialog)
