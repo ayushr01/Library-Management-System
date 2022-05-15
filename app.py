6a4e1440a4d35d8511160086df93b4c03f4a191d
Ayush Rao <ayushvijayrao@gmail.com>
2022-05-15 23:35:38 +0530

    App now uses roboto fonts

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+
+from PySide6.QtGui import QFontDatabase
 from PySide6.QtWidgets import QApplication
 
 import UI.primarywindow
@@ -5,7 +8,23 @@ import UI.primarywindow
 app = QApplication([])
 
 # Sets base size font to be same on all platforms (Required for cross-platform)
+# Also setting Ubuntu font
+home = os.path.dirname(__file__)
+QFontDatabase.addApplicationFont(home + '/Fonts/Roboto-Italic.ttf')
+QFontDatabase.addApplicationFont(home + '/Fonts/Roboto-Black.ttf')
+QFontDatabase.addApplicationFont(home + '/Fonts/Roboto-BlackItalic.ttf')
+QFontDatabase.addApplicationFont(home + '/Fonts/Roboto-Bold.ttf')
+QFontDatabase.addApplicationFont(home + '/Fonts/Roboto-BoldItalic.ttf')
+QFontDatabase.addApplicationFont(home + '/Fonts/Roboto-Light.ttf')
+QFontDatabase.addApplicationFont(home + '/Fonts/Roboto-LightItalic.ttf')
+QFontDatabase.addApplicationFont(home + '/Fonts/Roboto-Medium.ttf')
+QFontDatabase.addApplicationFont(home + '/Fonts/Roboto-MediumItalic.ttf')
+QFontDatabase.addApplicationFont(home + '/Fonts/Roboto-Regular.ttf')
+QFontDatabase.addApplicationFont(home + '/Fonts/Roboto-Thin.ttf')
+QFontDatabase.addApplicationFont(home + '/Fonts/Roboto-ThinItalic.ttf')
+
 font = app.font()
+font.setFamily('Roboto')
 font.setPixelSize(13)
 app.setFont(font)
 
