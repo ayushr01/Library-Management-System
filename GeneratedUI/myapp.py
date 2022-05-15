cda8f7577c57a7247bacee9949e8bb5cd9025ebf
Ayush Rao <ayushvijayrao@gmail.com>
2022-05-15 17:10:49 +0530

    Updated createpy script for new paths

--- GeneratedUI/myapp.py
+++ GeneratedUI/myapp.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading GeneratedUI file 'myapp.ui'
+## Form generated from reading UI file 'myapp.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.3.0
 ##
-## WARNING! All changes made in this file will be lost when recompiling GeneratedUI file!
+## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
