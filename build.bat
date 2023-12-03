4dc2abe846d38ec6e89487f026d83bcc36b5d777
Ayush Rao <ayushvijayrao@gmail.com>
2023-12-03 23:28:09 +0300

    Update windows build script

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-rm -rfv pyinstaller
+rmdir /s /q pyinstaller
 pyinstaller app.py -y --windowed --clean --name "LM System" --icon "icon.png" --distpath "./pyinstaller/dist" --workpath "./pyinstaller/build" --add-data "Assets:Assets"
